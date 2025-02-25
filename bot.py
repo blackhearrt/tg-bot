24d2aad1cc94c1e4df643ddc52dd1a815fb91c07
blackhearrt <svmamonov36@gmail.com>
2025-02-25 16:13:06 +0200

    Adding API request, improving minor functions

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, Message
 import asyncio
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv() 
@@ -36,6 +37,26 @@ async def currency_keyboard(message: Message):
     )
     await message.answer("Виберіть функцію:", reply_markup = keyboard)
 
+@dp.message(F.text == "💲 Отримати курс валюти")
+async def get_currency(message: types.Message):
+    url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        await message.answer("❌ Не вдалося отримати курс валют. Спробуйте пізніше.")
+        return
+    data = response.json()
+
+    currency_codes = ["USD", "EUR", "GBP", "PLN"]
+    currency_data = [currency for currency in data if currency["cc"] in currency_codes]
+
+    message_text = "💱 *Курс валют НБУ:*\n\n"
+    for currency in currency_data:
+         message_text += f"<b>{currency['txt']} ({currency['cc']}):</b> {currency['rate']} грн\n"
+
+    await message.answer(message_text, parse_mode="HTML")
+
+
 @dp.message(F.text == "✅ TODO-ліст")
 async def todo_keyboard(message: types.Message):
     keyboard = ReplyKeyboardMarkup(
@@ -47,9 +68,22 @@ async def todo_keyboard(message: types.Message):
     )
     await message.answer("Виберіть функцію:", reply_markup = keyboard)
 
+@dp.message(F.text == "⬅ Назад")
+async def back(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(
+        keyboard = [
+            [KeyboardButton(text = "📊 Курс валют")],
+            [KeyboardButton(text = "✅ TODO-ліст")],
+            [KeyboardButton(text = "Головне меню")]
+        ],
+        resize_keyboard= True
+    )
+    await message.answer("Виберіть функцію:", reply_markup = keyboard)
+
 async def main():
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
